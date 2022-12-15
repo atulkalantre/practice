45ade1d76cea023aaf8b7068321effae40885fd3
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2022-12-15 16:06:51 +0530

    3rd commit

--- Practice2/src/DoPractice/TestDemo.java
+++ Practice2/src/DoPractice/TestDemo.java
@@ -6,13 +6,7 @@ public class TestDemo {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hey World");
-		System.out.println("Hey World");
-		System.out.println("Hey World");
-		
-		System.out.println("Hey World");
-
-
-		System.out.println("Hey World");
+		System.out.println("Hey World");		
 
 	}
 
